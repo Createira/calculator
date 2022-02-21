de564ad9f1463a2a185db57185d3e2570550d5d3
Irina <ira.verbilovich@mail.ru>
2022-02-21 18:25:43 +0300

    init: 'start project'

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// Particles Library for background
+window.onload = function () {
+  Particles.init({
+    // normal options
+    selector: ".background",
+    maxParticles: 500,
+    // options for breakpoints
+    responsive: [
+      {
+        breakpoint: 768,
+        options: {
+          maxParticles: 250,
+          connectParticles: false,
+        },
+      },
+      {
+        breakpoint: 425,
+        options: {
+          maxParticles: 100,
+        },
+      },
+      {
+        breakpoint: 320,
+        options: {
+          maxParticles: 0,
+          // disables particles.js
+        },
+      },
+    ],
+  });
+};
+
+// Buttons
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    buttons.forEach((el) => {
+      el.classList.remove("active");
+    });
+    item.classList.add("active");
+  });
+});
